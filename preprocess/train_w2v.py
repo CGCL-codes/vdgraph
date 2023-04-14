ab6cd73c72994da947c7dd2cb597bc68cb02811a
vdgraph <125588323+vdgraph@users.noreply.github.com>
2023-04-14 11:45:59 +0800

    Update train_w2v.py

--- preprocess/train_w2v.py
+++ preprocess/train_w2v.py
@@ -117,13 +117,16 @@ def train_word_embedding(config_path: str):
     cweid = config.dataset.name
     root = config.data_folder
     #train_json = f"{root}/{cweid}/train.json"
-    train_path = "/home/mytest/nvd/all_slices"
+    train_path = "/home/Dataset/msr/dt_3/cfg/novul"
+    train_path2 = "/home/Dataset/msr/dt_3/cfg/vul"
+    paths2=glob.glob(train_path2+'/*')
     #with open(train_json, "r") as f:
         #paths = json.load(f)
     paths=glob.glob(train_path+'/*')
+    paths = paths + paths2
     tokens_list = list()
     with Manager():
-        pool = Pool(USE_CPU)
+        pool = Pool(8)
 
         process_func = functools.partial(process_parallel,
                                          split_token=config.split_token)
@@ -137,20 +140,20 @@ def train_word_embedding(config_path: str):
         ]
         pool.close()
         pool.join()
-    cnt=0
-    for token in tokens:
-        if token == []:
-            cnt+=1
-    print(cnt)
-    for token_l in tokens:
-        tokens_list.extend(token_l)
+    # cnt=0
+    # for token in tokens:
+    #     if token == []:
+    #         cnt+=1
+    # print(cnt)
+    # for token_l in tokens:
+    #     tokens_list.extend(token_l)
     print("training w2v...")
-    print(len(tokens_list))
+    print(len(tokens))
     num_workers = cpu_count(
     ) if config.num_workers == -1 else config.num_workers
-    model = Word2Vec(sentences=tokens_list, min_count=3, size=256,#vector_size=config.gnn.embed_size,
+    model = Word2Vec(sentences=tokens, min_count=3, seed=64, vector_size=config.gnn.embed_size,
                      max_vocab_size=config.dataset.token.vocabulary_size, workers=num_workers, sg=1)
-    model.wv.save("/home/deepwukong/w2v.wv")
+    model.wv.save("/home/Dataset/msr/dt_3/w2v_cfg.wv")
 
 
 def load_wv(config_path: str):
